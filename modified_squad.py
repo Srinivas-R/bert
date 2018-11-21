8eef34ee9fe05e79165320c069b6576c8ac33b3f
Srini <srini.shank@gmail.com>
2018-11-20 22:40:09 -0500

    2 sequences, one for doc and one for query

--- modified_squad.py
+++ modified_squad.py
@@ -201,24 +201,32 @@ class InputFeatures(object):
                unique_id,
                example_index,
                doc_span_index,
-               tokens,
+               doc_tokens,
+               query_tokens,
                token_to_orig_map,
                token_is_max_context,
-               input_ids,
-               input_mask,
-               segment_ids,
+               doc_input_ids,
+               doc_input_mask,
+               doc_segment_ids,
+               query_input_ids,
+               query_input_mask,
+               query_segment_ids,
                start_position=None,
                end_position=None,
                is_impossible=None):
     self.unique_id = unique_id
     self.example_index = example_index
     self.doc_span_index = doc_span_index
-    self.tokens = tokens
+    self.doc_tokens = doc_tokens
+    self.query_tokens = query_tokens
     self.token_to_orig_map = token_to_orig_map
     self.token_is_max_context = token_is_max_context
-    self.input_ids = input_ids
-    self.input_mask = input_mask
-    self.segment_ids = segment_ids
+    self.doc_input_ids = doc_input_ids
+    self.doc_input_mask = doc_input_mask
+    self.doc_segment_ids = doc_segment_ids
+    self.query_input_ids = query_input_ids
+    self.query_input_mask = query_input_mask
+    self.query_segment_ids = query_segment_ids
     self.start_position = start_position
     self.end_position = end_position
     self.is_impossible = is_impossible
@@ -314,7 +322,10 @@ def convert_examples_to_features(examples, tokenizer, max_seq_length,
   unique_id = 1000000000
 
   for (example_index, example) in enumerate(examples):
-    query_tokens = tokenizer.tokenize(example.question_text)
+    query_tokens = ["CLS"]
+    query_tokens.extend(tokenizer.tokenize(example.question_text))
+    query_tokens.append("[SEP]")
+    query_segment_ids = [0] * len(query_tokens)
 
     if len(query_tokens) > max_query_length:
       query_tokens = query_tokens[0:max_query_length]
@@ -344,8 +355,8 @@ def convert_examples_to_features(examples, tokenizer, max_seq_length,
           all_doc_tokens, tok_start_position, tok_end_position, tokenizer,
           example.orig_answer_text)
 
-    # The -3 accounts for [CLS], [SEP] and [SEP]
-    max_tokens_for_doc = max_seq_length - len(query_tokens) - 3
+    # The -2 accounts for [CLS], and [SEP]
+    max_tokens_for_doc = max_seq_length - 2
 
     # We can have documents that are longer than the maximum sequence length.
     # To deal with this we do a sliding window approach, where we take chunks
@@ -364,45 +375,58 @@ def convert_examples_to_features(examples, tokenizer, max_seq_length,
       start_offset += min(length, doc_stride)
 
     for (doc_span_index, doc_span) in enumerate(doc_spans):
-      tokens = []
+      doc_tokens = []
+      
       token_to_orig_map = {}
       token_is_max_context = {}
-      segment_ids = []
-      tokens.append("[CLS]")
-      segment_ids.append(0)
-      for token in query_tokens:
-        tokens.append(token)
-        segment_ids.append(0)
-      tokens.append("[SEP]")
-      segment_ids.append(0)
+      doc_segment_ids = []
+      doc_tokens.append("[CLS]")
+      doc_segment_ids.append(0)
+      # for token in query_tokens:
+      #   tokens.append(token)
+      #   segment_ids.append(0)
+      # tokens.append("[SEP]")
+      # segment_ids.append(0)
 
       for i in range(doc_span.length):
         split_token_index = doc_span.start + i
-        token_to_orig_map[len(tokens)] = tok_to_orig_index[split_token_index]
+        token_to_orig_map[len(doc_tokens)] = tok_to_orig_index[split_token_index]
 
         is_max_context = _check_is_max_context(doc_spans, doc_span_index,
                                                split_token_index)
-        token_is_max_context[len(tokens)] = is_max_context
-        tokens.append(all_doc_tokens[split_token_index])
-        segment_ids.append(1)
-      tokens.append("[SEP]")
-      segment_ids.append(1)
+        token_is_max_context[len(doc_tokens)] = is_max_context
+        doc_tokens.append(all_doc_tokens[split_token_index])
+        doc_segment_ids.append(0)
+      doc_tokens.append("[SEP]")
+      doc_segment_ids.append(0)
 
-      input_ids = tokenizer.convert_tokens_to_ids(tokens)
+      #input_ids = tokenizer.convert_tokens_to_ids(tokens)
+      doc_input_ids = tokenizer.convert_tokens_to_ids(doc_tokens)
+      query_input_ids = tokenizer.convert_tokens_to_ids(query_tokens)
 
       # The mask has 1 for real tokens and 0 for padding tokens. Only real
       # tokens are attended to.
-      input_mask = [1] * len(input_ids)
+      doc_input_mask = [1] * len(doc_input_ids)
+      query_input_mask = [1] * len(query_input_ids)
+
 
       # Zero-pad up to the sequence length.
-      while len(input_ids) < max_seq_length:
-        input_ids.append(0)
-        input_mask.append(0)
-        segment_ids.append(0)
+      while len(doc_input_ids) < max_seq_length:
+        doc_input_ids.append(0)
+        doc_input_mask.append(0)
+        doc_segment_ids.append(0)
+      wile len(query_input_ids) < max_seq_length:
+        query_input_ids.append(0)
+        query_input_mask.append(0)
+        query_segment_ids.append(0)
 
-      assert len(input_ids) == max_seq_length
-      assert len(input_mask) == max_seq_length
-      assert len(segment_ids) == max_seq_length
+      assert len(doc_input_ids) == max_seq_length
+      assert len(doc_input_mask) == max_seq_length
+      assert len(doc_segment_ids) == max_seq_length
+      assert len(query_input_ids) == max_seq_length
+      assert len(query_input_mask) == max_seq_length
+      assert len(query_segment_ids) == max_seq_length
+
 
       start_position = None
       end_position = None
@@ -418,7 +442,7 @@ def convert_examples_to_features(examples, tokenizer, max_seq_length,
           start_position = 0
           end_position = 0
         else:
-          doc_offset = len(query_tokens) + 2
+          doc_offset = 0 # len(query_tokens) + 2
           start_position = tok_start_position - doc_start + doc_offset
           end_position = tok_end_position - doc_start + doc_offset
 
@@ -426,23 +450,32 @@ def convert_examples_to_features(examples, tokenizer, max_seq_length,
         start_position = 0
         end_position = 0
 
-      if example_index < 20:
+      if example_index < 3:
         tf.logging.info("*** Example ***")
         tf.logging.info("unique_id: %s" % (unique_id))
         tf.logging.info("example_index: %s" % (example_index))
         tf.logging.info("doc_span_index: %s" % (doc_span_index))
-        tf.logging.info("tokens: %s" % " ".join(
-            [tokenization.printable_text(x) for x in tokens]))
+        tf.logging.info("doc tokens: %s" % " ".join(
+            [tokenization.printable_text(x) for x in doc_tokens]))
+        tf.logging.info("query tokens: %s" % " ".join(
+            [tokenization.printable_text(x) for x in query_tokens]))
         tf.logging.info("token_to_orig_map: %s" % " ".join(
             ["%d:%d" % (x, y) for (x, y) in six.iteritems(token_to_orig_map)]))
         tf.logging.info("token_is_max_context: %s" % " ".join([
             "%d:%s" % (x, y) for (x, y) in six.iteritems(token_is_max_context)
         ]))
-        tf.logging.info("input_ids: %s" % " ".join([str(x) for x in input_ids]))
+        tf.logging.info("doc_input_ids: %s" % " ".join([str(x) for x in doc input_ids]))
+        tf.logging.info(read_squad_examples
+            "doc_input_mask: %s" % " ".join([str(x) for x in doc_input_mask]))
         tf.logging.info(
-            "input_mask: %s" % " ".join([str(x) for x in input_mask]))
+            "doc_segment_ids: %s" % " ".join([str(x) for x in doc_segment_ids]))
+        
+        tf.logging.info("query_input_ids: %s" % " ".join([str(x) for x in query_input_ids]))
+        tf.logging.info(read_squad_examples
+            "query_input_mask: %s" % " ".join([str(x) for x in query_input_mask]))
         tf.logging.info(
-            "segment_ids: %s" % " ".join([str(x) for x in segment_ids]))
+            "query_segment_ids: %s" % " ".join([str(x) for x in query_segment_ids]))
+
         if is_training and example.is_impossible:
           tf.logging.info("impossible example")
         if is_training and not example.is_impossible:
@@ -456,12 +489,16 @@ def convert_examples_to_features(examples, tokenizer, max_seq_length,
           unique_id=unique_id,
           example_index=example_index,
           doc_span_index=doc_span_index,
-          tokens=tokens,
+          doc_tokens=doc_tokens,
+          query_tokens=query_tokens,
           token_to_orig_map=token_to_orig_map,
           token_is_max_context=token_is_max_context,
-          input_ids=input_ids,
-          input_mask=input_mask,
-          segment_ids=segment_ids,
+          doc_input_ids=doc_input_ids,
+          doc_input_mask=doc_input_mask,
+          doc_segment_ids=doc_segment_ids,
+          query_input_ids=query_input_ids,
+          query_input_mask=query_input_mask,
+          query_segment_ids=query_segment_ids,
           start_position=start_position,
           end_position=end_position,
           is_impossible=example.is_impossible)
@@ -546,33 +583,54 @@ def _check_is_max_context(doc_spans, cur_span_index, position):
   return cur_span_index == best_span_index
 
 
-def create_model(bert_config, is_training, input_ids, input_mask, segment_ids,
-                 use_one_hot_embeddings):
+def create_model(bert_config, is_training, doc_input_ids, 
+                 query_input_ids, doc_input_mask, query_input_mask, 
+                 doc_segment_ids, query_segment_ids, use_one_hot_embeddings):
   """Creates a classification model."""
-  model = modeling.BertModel(
+
+  model_doc = modeling.BertModel(
       config=bert_config,
       is_training=is_training,
-      input_ids=input_ids,
-      input_mask=input_mask,
-      token_type_ids=segment_ids,
+      input_ids=doc_input_ids,
+      input_mask=doc_input_mask,
+      token_type_ids=doc_segment_ids,
       use_one_hot_embeddings=use_one_hot_embeddings)
 
-  final_hidden = model.get_sequence_output()
+  model_query = modeling.BertModel(
+      config=config,
+      is_training=is_training,
+      input_id=query_input_ids,
+      input_mask=query_input_mask,
+      token_type_ids=query_segment_ids,
+      use_one_hot_embeddings=use_one_hot_embeddings)
 
-  final_hidden_shape = modeling.get_shape_list(final_hidden, expected_rank=3)
-  batch_size = final_hidden_shape[0]
-  seq_length = final_hidden_shape[1]
-  hidden_size = final_hidden_shape[2]
+  doc_final_hidden = model_doc.get_sequence_output()
 
+  doc_hidden_shape = modeling.get_shape_list(doc_final_hidden, expected_rank=3)
+  batch_size = doc_hidden_shape[0]
+  seq_length = doc_hidden_shape[1]
+  hidden_size = doc_hidden_shape[2]
+
+  
   output_weights = tf.get_variable(
-      "cls/squad/output_weights", [2, hidden_size],
+      "cls/squad/output_weights", [2, 2 * hidden_size],
       initializer=tf.truncated_normal_initializer(stddev=0.02))
 
   output_bias = tf.get_variable(
       "cls/squad/output_bias", [2], initializer=tf.zeros_initializer())
 
-  final_hidden_matrix = tf.reshape(final_hidden,
+  doc_hidden_matrix = tf.reshape(doc_final_hidden,
                                    [batch_size * seq_length, hidden_size])
+  #need only representation of [CLS] token of query
+  #this is size [batch_size, 1, hidden_size]
+  query_rep = model_query.get_sequence_output()[:, 0:1, :]
+  #tile it to concatenate it with each doc token repr
+  tiled_query_rep = tf.tile(query_rep, [1, seq_length, 1])
+  
+  #shape is [batch_size, seq_length, 2*hidden_size]
+  final_hidden_matrix = tf.concatenate([doc_hidden_matrix, tiled_query_rep], \
+                                      axis=2)
+
   logits = tf.matmul(final_hidden_matrix, output_weights, transpose_b=True)
   logits = tf.nn.bias_add(logits, output_bias)
 
@@ -599,18 +657,24 @@ def model_fn_builder(bert_config, init_checkpoint, learning_rate,
       tf.logging.info("  name = %s, shape = %s" % (name, features[name].shape))
 
     unique_ids = features["unique_ids"]
-    input_ids = features["input_ids"]
-    input_mask = features["input_mask"]
-    segment_ids = features["segment_ids"]
+    doc_input_ids = features["doc_input_ids"]
+    doc_input_mask = features["doc_input_mask"]
+    doc_segment_ids = features["doc_segment_ids"]
+    query_input_ids = features["query_input_ids"]
+    query_input_mask = features["query_input_mask"]
+    query_segment_ids = features["query_segment_ids"]
 
     is_training = (mode == tf.estimator.ModeKeys.TRAIN)
 
     (start_logits, end_logits) = create_model(
         bert_config=bert_config,
         is_training=is_training,
-        input_ids=input_ids,
-        input_mask=input_mask,
-        segment_ids=segment_ids,
+        doc_input_ids=doc_input_ids,
+        doc_input_mask=doc_input_mask,
+        doc_segment_ids=doc_segment_ids,
+        query_input_ids=query_input_ids,
+        query_input_mask=query_input_mask,
+        query_segment_ids=query_segment_ids,
         use_one_hot_embeddings=use_one_hot_embeddings)
 
     tvars = tf.trainable_variables()
@@ -640,7 +704,7 @@ def model_fn_builder(bert_config, init_checkpoint, learning_rate,
 
     output_spec = None
     if mode == tf.estimator.ModeKeys.TRAIN:
-      seq_length = modeling.get_shape_list(input_ids)[1]
+      seq_length = modeling.get_shape_list(doc_input_ids)[1]
 
       def compute_loss(logits, positions):
         one_hot_positions = tf.one_hot(
@@ -688,9 +752,12 @@ def input_fn_builder(input_file, seq_length, is_training, drop_remainder):
 
   name_to_features = {
       "unique_ids": tf.FixedLenFeature([], tf.int64),
-      "input_ids": tf.FixedLenFeature([seq_length], tf.int64),
-      "input_mask": tf.FixedLenFeature([seq_length], tf.int64),
-      "segment_ids": tf.FixedLenFeature([seq_length], tf.int64)
+      "doc_input_ids": tf.FixedLenFeature([seq_length], tf.int64),
+      "doc_input_mask": tf.FixedLenFeature([seq_length], tf.int64),
+      "doc_segment_ids": tf.FixedLenFeature([seq_length], tf.int64),
+      "query_input_ids": tf.FixedLenFeature([seq_length], tf.int64),
+      "query_input_mask": tf.FixedLenFeature([seq_length], tf.int64),
+      "query_segment_ids": tf.FixedLenFeature([seq_length], tf.int64)
   }
 
   if is_training:
@@ -918,7 +985,7 @@ def write_predictions(all_examples, all_features, all_results, n_best_size,
   with tf.gfile.GFile(output_nbest_file, "w") as writer:
     writer.write(json.dumps(all_nbest_json, indent=4) + "\n")
 
-  if FLAGS.version_2_with_negative:
+  if FLAGS.version_2_with_negative:example_index
     with tf.gfile.GFile(output_null_log_odds_file, "w") as writer:
       writer.write(json.dumps(scores_diff_json, indent=4) + "\n")
 
@@ -1074,9 +1141,12 @@ class FeatureWriter(object):
 
     features = collections.OrderedDict()
     features["unique_ids"] = create_int_feature([feature.unique_id])
-    features["input_ids"] = create_int_feature(feature.input_ids)
-    features["input_mask"] = create_int_feature(feature.input_mask)
-    features["segment_ids"] = create_int_feature(feature.segment_ids)
+    features["doc_input_ids"] = create_int_feature(feature.doc_input_ids)
+    features["doc_input_mask"] = create_int_feature(feature.doc_input_mask)
+    features["doc_segment_ids"] = create_int_feature(feature.doc_segment_ids)
+    features["query_input_ids"] = create_int_feature(feature.query_input_ids)
+    features["query_input_mask"] = create_int_feature(feature.query_input_mask)
+    features["query_segment_ids"] = create_int_feature(feature.query_segment_ids)
 
     if self.is_training:
       features["start_positions"] = create_int_feature([feature.start_position])
